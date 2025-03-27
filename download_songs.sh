f754404fa12a2ce43c7ac77584c7e669e3bc227f
ViniiR <cvrm2004@gmail.com>
2025-03-26 23:11:42 -0300

    feat: add all playlists
    add a few not found songs

--- download_songs.sh
+++ download_songs.sh
@@ -1,9 +1,39 @@
 #!/usr/bin/env bash
 
-while IFS= read -r line; do
-    if [[ "$line" == *"#"* ]]; then
-        continue
-    fi
-    yt-dlp -f bestaudio --extract-audio --audio-format opus --progress -o "/home/vinii/projects/Project-Reconquista/downloaded_songs/%(title)s.%(ext)s" "${line//\,/}"
-done < ./song_list.csv
+songs_filepath="/home/vinii/projects/Project-Reconquista/downloaded_songs/%(title)s.%(ext)s"
+playlists_filepath="/home/vinii/projects/Project-Reconquista/downloaded_songs/playlists/%(playlist_title)/%(title)s.%(ext)s"
+
+case "$1" in
+    "--songs")
+        while IFS= read -r line; do
+            if [[ "$line" == *"#"* ]]; then
+                continue
+            fi
+            yt-dlp -f bestaudio --extract-audio --audio-format opus --progress -o "$songs_filepath" "${line//\,/}"
+        done < ./song_list.csv
+    ;;
+    "--playlists")
+        next_is_helldivers=false
+        while IFS= read -r line; do
+            if [[ "$line" == *"#"* ]]; then
+                next_is_helldivers=false
+                if [[ "$line" == *"Helldivers"* ]]; then
+                    next_is_helldivers=true
+                fi
+                continue
+            fi
+            if [[ "$next_is_helldivers" == true ]]; then
+                yt-dlp --match-filter "title~=.*Original Game Soundtrack.*" -f bestaudio --extract-audio --audio-format opus --progress -o "$playlists_filepath" "${line//\,/}"
+                next_is_helldivers=false
+                continue
+            fi
+            yt-dlp -f bestaudio --extract-audio --audio-format opus --progress -o "$playlists_filepath" "${line//\,/}"
+        done < ./playlists_list.csv
+    ;;
+    *) 
+        echo "No option specified"
+        exit 0
+    ;;
+esac
+
 
